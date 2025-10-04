b4002eb8825772d743c88bed761946ed8b992a37
Johannes Osterkamp <j.osterkamp@lms-sport.de>
2025-10-04 21:37:17 +0200

    Hide title in mobile view

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,17 +3,14 @@ import { useState } from "react";
 import { CartButton } from "./cart-button";
 import { CartDrawer } from "./cart-drawer";
 import AuthButton from "./auth-button";
-import { useUser } from "@clerk/nextjs";
-import Link from "next/link";
 import { MainNav } from "./main-nav";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
-  const { user } = useUser();
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between bg-white px-4 py-3 shadow">
       <div className="flex items-center gap-8">
-        <h1 className="text-xl font-bold">Product Browser</h1>
+        <h1 className="hidden text-xl font-bold sm:block">Product Browser</h1>
         <MainNav />
       </div>
       <div className="grid grid-flow-col items-center gap-5">
